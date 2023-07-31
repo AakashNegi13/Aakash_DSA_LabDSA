846b0dd00bb5481bd0dba3c3ecba6e1c6a80515f
AakashNegi13 <135734604+AakashNegi13@users.noreply.github.com>
2023-08-01 00:11:19 +0530

    Add files via upload

---
+++ Great Learning - Lab 3 DSA/BST_Sum/src/com/greatlearning/binarySearchTree/MyNode.java
@@ -0,0 +1,14 @@
+package com.greatlearning.binarySearchTree;
+
+public class MyNode {
+	MyNode left;
+	int data;
+	MyNode right;
+	
+	public MyNode(int data) {
+		left = null;
+		this.data=data;
+		right = null;
+	}
+
+}
---
+++ Great Learning - Lab 3 DSA/Brackets/src/com/greatlearning/balancingBrackets/DriverApp.java
@@ -0,0 +1,17 @@
+package com.greatlearning.balancingBrackets;
+
+public class DriverApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String input = "{[()]}";
+		MyStack ms = new MyStack();
+		Boolean result = ms.checkBracket(input);
+		if (result == true)
+			System.out.println("Valid Expression");
+		else
+			System.out.println("Invalid Expression");
+
+	}
+
+}
